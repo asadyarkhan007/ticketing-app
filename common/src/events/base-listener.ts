84d54cf7ba600060aa54d08c553cf5cab25439c5
AsadKhan <asadyarkhan007@gmail.com>
2024-02-11 22:20:34 -0330

    Updates

---
+++ common/src/events/base-listener.ts
@@ -0,0 +1,73 @@
+import {
+  StringCodec,
+  JsMsg,
+  AckPolicy,
+  JetStreamManager,
+  DeliverPolicy,
+} from "nats";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract stream: string;
+  abstract subject: T["subject"];
+  abstract consumerName: string;
+  abstract onMessage(data: T["data"], msg: JsMsg): void;
+  private jsm: JetStreamManager;
+  protected ackWait = 5 * 1000;
+
+  constructor(jsm: JetStreamManager) {
+    this.jsm = jsm;
+  }
+
+  consumerConfig() {
+    return {
+      deliver_policy: DeliverPolicy.All,
+      ack_policy: AckPolicy.Explicit,
+      ack_wait: this.ackWait,
+      durable_name: this.consumerName,
+    };
+  }
+
+  streamConfig() {
+    return {
+      name: this.stream,
+      subjects: [this.subject],
+    };
+  }
+
+  async listen() {
+    try {
+      await this.jsm.consumers.info(this.stream, this.consumerName);
+    } catch (err) {
+      await this.jsm.consumers.add(this.stream, this.consumerConfig());
+    }
+
+    await this.jsm.streams.add(this.streamConfig());
+    const jsclient = this.jsm.jetstream();
+    const consumer = await jsclient.consumers.get(
+      this.stream,
+      this.consumerName
+    );
+    const messages = await consumer.consume();
+    for await (const m of messages) {
+      console.log(
+        `Message received: ${this.subject} / ${this.consumerName} sequence: ${m.seq} message: ${m.data}`
+      );
+      const parsedData = this.parseMessage(m);
+      this.onMessage(parsedData, m);
+    }
+  }
+
+  parseMessage(msg: JsMsg) {
+    const data = msg.data;
+    const sc = StringCodec();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(sc.decode(data));
+  }
+}
---
+++ common/src/events/base-publisher.ts
@@ -0,0 +1,38 @@
+import {
+  StringCodec,
+  JsMsg,
+  AckPolicy,
+  JetStreamManager,
+  DeliverPolicy,
+  PubAck,
+} from "nats";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+  abstract subject: T["subject"];
+  abstract stream: string;
+  private jsm: JetStreamManager;
+
+  constructor(jsm: JetStreamManager) {
+    this.jsm = jsm;
+  }
+
+  streamConfig(): any {
+    return {
+      name: this.stream,
+      subjects: [this.subject],
+    };
+  }
+
+  async publish(data: T["data"]): Promise<PubAck> {
+    await this.jsm.streams.add(this.streamConfig());
+    const js = this.jsm.jetstream();
+    const sc = StringCodec();
+    return js.publish(this.subject, JSON.stringify(data));
+  }
+}
