2f8d3822932170e2f0ec2d2750e796a7f39a01d6
AsadKhan <asadyarkhan007@gmail.com>
2024-02-28 09:51:53 -0330

    update

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -12,7 +12,10 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly consumerName = "order-service";
 
   async onMessage(data: TicketCreatedEvent["data"], msg: JsMsg) {
+    console.log("ackdolwge messege soon");
+
     const { id, title, price } = data;
+    console.log(`${data.id}`);
     const ticketExist = await Ticket.findById(id);
     if (!ticketExist) {
       const ticket = Ticket.build({
@@ -20,7 +23,6 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
         title,
         price,
       });
-
       await ticket.save();
     }
     msg.ack();
