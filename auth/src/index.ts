df97b73a7c789f594ae7ed12eae8712fcef93bd5
AsadKhan <asadyarkhan007@gmail.com>
2024-03-31 13:14:21 -0230

    update changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
-  console.log("Auth starting up");
+  console.log("Auth service starting up");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
